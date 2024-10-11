00f148c17da7ae785b71f6f160d65758ad664b2e
Aayush <aayushmotiani1234@gmail.com>
2024-10-10 23:19:07 -0700

    lab2

---
+++ Lab2/my-app/src/App.tsx
@@ -0,0 +1,168 @@
+import { dummyNotesList } from "./constants";
+import './App.css';
+import { ToggleTheme } from "./hooksExercise";
+import React, { SetStateAction, useState } from 'react';
+import { Label } from "./types";
+
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+  label: Label;
+  favorite: string;
+}
+
+function App() {
+
+  //usestate initialization of: 1) exampleNotesList array using dummy notes list, 2) favoritesList array which contains "id"s of only fav notes
+  const [favoritesList, setFavoritesList] = useState<number[]>([]); 
+  
+  //make a function that handles adding/removing elements of fav list, check if favlist includes that element, you remove it else add it. 
+  //and pass a Note element as parameter.
+  // then pass this function to the FavoritesFlag() function and invoke it whenever our favorites button is clicked.
+  const checkFav = (note:Note) => {
+    if(favoritesList.includes(note.id)){
+      //remove
+      setFavoritesList(favoritesList.filter(item => item!=note.id));
+    }
+    else{
+      setFavoritesList([...favoritesList, note.id]);
+    }
+  };
+
+  //initialize notes, setnotes with dummynoteslist
+  const [notes, setNotes] = useState(dummyNotesList);
+
+  const initialNote = {
+    id: 7,
+    title: "",
+    content: "",
+    label: Label.other,
+    favorite: "♡"
+  };
+
+  //initialize createNote and setCreateNote with initialNote, an example note value 
+  const [createNote, setCreateNote] = useState(initialNote);
+
+  //Create a new sticky note and display it (runs when submit button is clicked)
+  const createNoteHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()  //prevents reloading
+    
+    createNote.id++;
+    //update notes list's value with new note added
+    setNotes([...notes, createNote]);
+    //change new note's id
+  }
+
+  //EDIT:
+  const [selectedNote, setSelectedNote] = useState<Note>(initialNote);
+  const editNoteHandler = () => {
+    //update note's field which was edited and reflect changes in the notes list 
+    //maybe use useEffect
+    
+  }
+
+  //DELETE NOTE:
+  const deleteNote = (noteToDelete: Note) => {
+    if(notes.includes(noteToDelete)){
+    setNotes(notes.filter(item => item!=noteToDelete))
+    }
+  }
+
+ return (
+  <div className='app-container'>
+  <form className="note-form" onSubmit={createNoteHandler}>
+    <div>
+      <input
+        placeholder="Note Title"
+        onChange={(event) =>
+          setCreateNote({ ...createNote, title: event.target.value })}
+        required>
+      </input>
+    </div>
+
+    <div>
+      <textarea
+        onChange={(event) =>
+          setCreateNote({ ...createNote, content: event.target.value })}
+        required>
+      </textarea>
+    </div>
+
+    <div>
+     <select
+       onChange={(event) =>
+         setCreateNote({ ...createNote, label: event.target.value as Label})}
+       required>
+       <option value={Label.personal}>Personal</option>
+       <option value={Label.study}>Study</option>
+       <option value={Label.work}>Work</option>
+       <option value={Label.other}>Other</option>
+     </select>
+   </div>
+
+    <div><button type="submit">Create Note</button></div>
+  </form>
+  
+    <div className="notes-grid">
+       {notes.map((note) => (
+         <div
+           key={note.id}
+           className="note-item">
+           <div className="notes-header">
+           <FavoritesFlag favState = {note.favorite} toggleFavoriteChange={checkFav} selectedNote = {note}/>
+           <button onClick={()=>deleteNote(note)}>x</button>
+          </div>
+            <h2 contentEditable="true" onInput={editNoteHandler}> {note.title} </h2>
+            <p contentEditable="true"> {note.content} </p>
+            <p contentEditable="true"> {note.label} </p>
+          </div>
+       ))}
+     </div>
+  <ToggleTheme/>
+    <div>
+      List of Favorites:
+      {/*
+        HERE, FILTER OUT ELEMENTS FROM NOTES LIST, TO CHECK IF THEY ALSO EXIST IN FAV NOTES LIST. IF SO, DISPLAY THE ELEMENTS OF
+        NOTES LIST WHOSE ID IS IN FAVNOTES LIST
+      */}
+      <ul>
+          {notes.filter(note => favoritesList.includes(note.id))
+            .map((note) => (
+              <li key={note.id}>{note.title}</li>
+            ))
+          }
+      </ul>
+     </div>
+   </div>
+
+ );
+}
+
+export default App;
+
+interface FavFlagsInterface {
+  favState: string;
+  selectedNote: Note;
+  toggleFavoriteChange: (note: Note) => void;
+}
+
+//Fav List Update Function updates the List of favs dynamically
+export function FavoritesFlag({ favState, selectedNote, toggleFavoriteChange }: FavFlagsInterface) {
+  const [like, setLike] = useState(favState);
+
+  const toggleLike = () => {
+    setLike(like === '♡' ? '❤️' : '♡');
+    toggleFavoriteChange(selectedNote);
+  };
+  
+  return (
+    <div>
+    <button onClick={toggleLike}>
+        {like}
+      </button>
+   </div>
+  );
+}
+
+ 
---
+++ Lab2/my-app/src/constants.ts
@@ -0,0 +1,92 @@
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+
+export const dummyNotesList = [
+    {
+        id: 1,
+        title: "note 1 title",
+        content: "test note 1 content",
+        label: Label.other,
+        favorite: '♡'
+    },
+    {
+        id: 2,
+        title: "note 2 title",
+        content: "test note 2 content",
+        label: Label.personal,
+        favorite: '♡'
+    },
+    {
+        id: 3,
+        title: "note 3 title",
+        content: "test note 3 content",
+        label: Label.work,
+        favorite: '♡'
+    },
+    {
+        id: 4,
+        title: "note 4 title",
+        content: "test note 4 content",
+        label: Label.study,
+        favorite: '♡'
+    },
+    {
+        id: 5,
+        title: "note 5 title",
+        content: "test note 5 content",
+        label: Label.study,
+        favorite: '♡'
+    },
+    {
+        id: 6,
+        title: "note 6 title",
+        content: "test note 6 content",
+        label: Label.personal,
+        favorite: '♡'
+    },
+ ]
+
+
+ export const copyNotesList = [
+    {
+        id: 1,
+        title: "note 1 title",
+        content: "test note 1 content",
+        label: Label.other,
+        favorite: '♡'
+    },
+    {
+        id: 2,
+        title: "note 2 title",
+        content: "test note 2 content",
+        label: Label.personal,
+        favorite: '♡'
+    },
+    {
+        id: 3,
+        title: "note 3 title",
+        content: "test note 3 content",
+        label: Label.work,
+        favorite: '♡'
+    },
+    {
+        id: 4,
+        title: "note 4 title",
+        content: "test note 4 content",
+        label: Label.study,
+        favorite: '♡'
+    },
+    {
+        id: 5,
+        title: "note 5 title",
+        content: "test note 5 content",
+        label: Label.study,
+        favorite: '♡'
+    },
+    {
+        id: 6,
+        title: "note 6 title",
+        content: "test note 6 content",
+        label: Label.personal,
+        favorite: '♡'
+    },
+ ]
